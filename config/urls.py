1d045e05f76caf1a65cd336cf63c397b764b0311
Lomke <lavzd13@gmail.com>
2026-03-01 12:08:26 +0100

    fixed issue with picture access

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+
+
+@login_required
+def protected_media(request, path):
+    return serve(request, path, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.api_urls')),
     path('', include('accounts.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', protected_media),
 ]
