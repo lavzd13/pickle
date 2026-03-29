69ffa1c02ee03d6aa4dc13d154578f3137ba8ce4
Lomke <lavzd13@gmail.com>
2026-03-29 12:58:18 +0200

    update

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        from django.db.models.signals import pre_delete
+        from django.dispatch import receiver
+
+        @receiver(pre_delete, sender=User)
+        def prevent_superuser_delete(sender, instance, **kwargs):
+            if instance.is_superuser:
+                raise PermissionError(f'Cannot delete superuser "{instance.username}". Remove superuser status first.')
